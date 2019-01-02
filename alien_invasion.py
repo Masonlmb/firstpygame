5c856628705f81e12a74e1a8b5765e16b1ba5138
lmb <lmbbehappy@gmail.com>
2019-01-02 15:43:46 +1300

    add first file

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 
 
 def run_game():
@@ -15,15 +16,16 @@ def run_game():
 
     #creat ship
     ship =  Ship(ai_settings, screen)
-
+    #creat bullets group
+    bullets = Group()
     #start game main loop
 
     while True:
 
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        print(ship.rect.centerx)
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 
--- game_functions.py
+++ game_functions.py
@@ -1,16 +1,18 @@
 import sys
-
+from Bullet import Bullet
 import pygame
 
 
 
 
-def check_keydown_events(event,ship):
+def check_keydown_events(event,ai_settings, screen, ship, bullets):
     if event.key == pygame.K_RIGHT:
         # ship move right
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        fire_bullet(ai_settings, screen, ship, bullets)
 
 
 def check_keyup_events(event,ship):
@@ -20,27 +22,41 @@ def check_keyup_events(event,ship):
         ship.moving_left = False
 
 
-def check_events(ship):
+def check_events(ai_setting, screen, ship, bullets):
     '''response for keyboard and mouse action'''
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event,ship)
+            check_keydown_events(event,ai_setting, screen, ship, bullets)
 
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
 
 
+def fire_bullet(ai_settings, screen, ship, bullets):
+    # creat new bullet and added in bullets
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 
-def update_screen(ai_setting, screen, ship):
+def update_screen(ai_setting, screen, ship, bullets):
     """update screen"""
 
     screen.fill(ai_setting.bg_color)
     ship.blitme()
-
+    for bullet in bullets:
+        bullet.draw_bullet()
     pygame.display.flip()
 
+def update_bullets(bullets):
+    '''update bullet position and number and delete outside bullet'''
+
+    bullets.update()
+
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
